b5a121b3fc1e2504f71719cf774d0cea15c1f200
Lazuta <lenya.lazuta@mail.ru>
2020-08-18 10:53:23 +0300

    Fix output

--- app/Http/Controllers/HelpController.php
+++ app/Http/Controllers/HelpController.php
@@ -10,7 +10,7 @@ class HelpController extends Controller
 {
     public function index()
     {
-        $users = User::where('public', true)->where('status', 'active')->get();
+        $users = User::where('public', true)->where('status', 'active')->where('status', 'work')->get();
         $roles = Role::get()->all();
 
         return view('help', [
